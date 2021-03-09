7a3ea8aa5b43958da8b0b04c2f172f2ccec52753
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-03-09 12:39:51 -0700

    Model tested best discount method for invoice_item model

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -12,4 +12,19 @@ RSpec.describe InvoiceItem, type: :model do
     it { should belong_to :invoice }
     it { should belong_to :item }
   end
+
+  describe "instance methods" do
+    it "find best discount" do
+      BulkDiscount.destroy_all
+      @merchant1 = Merchant.create!(name: 'Hair Care')
+      @bulk_discount1 = BulkDiscount.create!(percentage: 0.3, minimum: 8, merchant_id: @merchant1.id)
+      @bulk_discount2 = BulkDiscount.create!(percentage: 0.4, minimum: 7, merchant_id: @merchant1.id)
+      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 8, unit_price: 10, status: 2)
+
+      expect(@ii_1.find_best_discount).to eq(@bulk_discount2.id)
+    end
+  end
 end
