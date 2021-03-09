0e2358e62777219dbd64a3808f6addcd0b0fee1d
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-03-09 09:35:24 -0700

    Update gems to push to heroku

--- app/services/api_service.rb
+++ app/services/api_service.rb
@@ -1,8 +1,8 @@
-require "faraday"
-class ApiService
-	def self.get_data(endpoint)
-		response = Faraday.get(endpoint)
-		data = response.body
-		JSON.parse(data, symbolize_names: true)
-	end
-end
+# require "faraday"
+# class ApiService
+# 	def self.get_data(endpoint)
+# 		response = Faraday.get(endpoint)
+# 		data = response.body
+# 		JSON.parse(data, symbolize_names: true)
+# 	end
+# end
