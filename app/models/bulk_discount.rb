ebec0c6200129edeacb1470474ab0f3aa032a29e
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-03-09 22:00:58 -0700

    Add sad path code for create and update discount

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,6 +2,9 @@ class BulkDiscount < ApplicationRecord
   validates_presence_of :merchant_id,
                         :percentage,
                         :minimum
+  validates_numericality_of :percentage, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 1
+  validates_numericality_of :minimum, :greater_than_or_equal_to => 1
+
   belongs_to :merchant
 
 
