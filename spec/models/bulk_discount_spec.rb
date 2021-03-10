57d9c920989464f3720bfcbb8de3cc81e8d1385d
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-03-09 22:43:03 -0700

    Add validation tests to discount model

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -6,6 +6,9 @@ describe BulkDiscount do
     it { should validate_presence_of :merchant_id }
     it { should validate_presence_of :percentage }
     it { should validate_presence_of :minimum }
+    it { should validate_numericality_of(:percentage).is_greater_than_or_equal_to(0)}
+    it { should validate_numericality_of(:percentage).is_less_than_or_equal_to(1)}
+    it { should validate_numericality_of(:minimum).is_greater_than_or_equal_to(1)}
   end
 
   describe "relationships" do
