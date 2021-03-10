3b8fc311976c39c32d0ddf840cb57a2dfb630d29
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-03-09 19:45:28 -0700

    Add discount show page link to invoice show spec

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -113,7 +113,8 @@ RSpec.describe 'invoices show' do
 
      expect(page).to have_link("Discount ##{@ii_1.find_best_discount}")
      expect(page).to have_link("Discount ##{@ii_11.find_best_discount}")
-     # click_link("Discount ##{@ii_1.find_best_discount}")
+     click_link("Discount ##{@ii_1.find_best_discount}")
+     expect(current_path).to eq("/merchant/#{@merchant1.id}/bulk_discounts/#{@ii_11.find_best_discount}")
   end
 
 end
